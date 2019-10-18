d3337c82cc4b9c6c5c697c68bba5d2b47fd3e264
Javier Jareno Diaz <javier@boardvitals.com>
2019-10-18 10:53:36 -0400

    - Fixes to remove Select All button in iOS

--- ios/RNCHighlightWebView.m
+++ ios/RNCHighlightWebView.m
@@ -18,31 +18,37 @@
                                                    object:nil];
         [[NSClassFromString(@"UICalloutBarButton") appearance] setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [[NSClassFromString(@"UICalloutBarButton") appearance] setBackgroundColor:[UIColor colorWithRed:22.0f/255.0f green:110.0f/255.0f blue:213.0f/255.0f alpha:1.0f]];
+        UIMenuItem *menuItem = [[UIMenuItem alloc] initWithTitle:@"Highlight"
+                                                          action:@selector(highlight:)];
+        [[UIMenuController sharedMenuController] setMenuItems:[NSArray arrayWithObjects:menuItem,nil]];
         
     }
     return [super initWithFrame:frame configuration:configuration];
 }
 
 - (void)didReceiveMenuWillShowNotification:(NSNotification *)notification {
-    
     [[NSNotificationCenter defaultCenter] removeObserver:self
                                                     name:UIMenuControllerWillShowMenuNotification
                                                   object:nil];
     
     UIMenuController *menu = [notification object];
-    [menu setMenuVisible:NO animated:NO];
+    CGRect frame = menu.menuFrame;
     [self evaluateJavaScript:@"document.queryCommandValue('backcolor')" completionHandler:^(id _Nullable currentColor, NSError * _Nullable error) {
         if ([currentColor isEqualToString:@"rgba(0, 0, 0, 0)"]) { // Not Highlighted => Adding highlight
             UIMenuItem *menuItem = [[UIMenuItem alloc] initWithTitle:@"Highlight"
                                                               action:@selector(highlight:)];
             [menu setMenuItems:[NSArray arrayWithObjects:menuItem,nil]];
-            [menu setMenuVisible:YES animated:NO];
+            [menu setArrowDirection:UIMenuControllerArrowUp];
+            [menu setTargetRect:frame inView:self];
+            [menu update];
         }
         else{ // Highlighted => Removing highlight
             UIMenuItem *menuItem2 = [[UIMenuItem alloc] initWithTitle:@"Remove Highlight"
                                                                action:@selector(removeHighlight:)];
             [menu setMenuItems:[NSArray arrayWithObjects:menuItem2,nil]];
-            [menu setMenuVisible:YES animated:NO];
+            [menu setArrowDirection:UIMenuControllerArrowUp];
+            [menu setTargetRect:frame inView:self];
+            [menu update];
         }
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(didReceiveMenuWillShowNotification:)
@@ -56,7 +62,7 @@
 - (BOOL)canPerformAction:(SEL)action withSender:(id)sender
 {
     if (self.highlightEnabled){
-        if (action == @selector(selectAll:) || action == @selector(highlight:) || action == @selector(removeHighlight:)) {
+        if (action == @selector(highlight:) || action == @selector(removeHighlight:)) {
             return YES;
         }
         return NO;
@@ -68,11 +74,11 @@
 
 - (void) highlight:(id)sender  {
     [self evaluateJavaScript:@"highlightSelectedText()" completionHandler:^(id _Nullable newHtml, NSError * _Nullable error) {
-         [self evaluateJavaScript:@"getHighlights()" completionHandler:^(id _Nullable highlights, NSError * _Nullable error) {
+        [self evaluateJavaScript:@"getHighlights()" completionHandler:^(id _Nullable highlights, NSError * _Nullable error) {
             if (self.delegate != nil) {
                 [self.delegate htmlContentChanged:newHtml ranges:highlights];
             }
-         }];
+        }];
     }];
 }
 
